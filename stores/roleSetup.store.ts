2126213883f66c97af03ff47dfae8b6a85ea98ea
SonyaJAVASCRIPT <claireroush13iq@gmail.com>
2026-01-14 20:31:53 +0100

    fix: fix comments + line fix

--- stores/roleSetup.store.ts
+++ stores/roleSetup.store.ts
@@ -4,24 +4,22 @@ import path from "path";
 const FILE_PATH = path.join(process.cwd(), "reactionRoles.json");
 
 export type ReactionRoleCollection = {
-  [collection: string]: { [emoji: string]: string }; // emoji → roleId
+  [collection: string]: { [emoji: string]: string };
 };
 
 export type ReactionRoleMessages = {
-  [messageId: string]: string; // messageId → collection
+  [messageId: string]: string;
 };
 
 export let reactionRoleBinds: ReactionRoleCollection = {};
 export let reactionRoleMessages: ReactionRoleMessages = {};
 
-// загрузка при старте
 if (fs.existsSync(FILE_PATH)) {
   const data = JSON.parse(fs.readFileSync(FILE_PATH, "utf-8"));
   reactionRoleBinds = data.reactionRoleBinds || {};
   reactionRoleMessages = data.reactionRoleMessages || {};
 }
 
-// функция сохранения
 export function saveReactionRoles() {
   fs.writeFileSync(
     FILE_PATH,
